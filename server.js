11394a60d8890f5113651fda4f51a4940f35acba
Max Wofford <max@maxwofford.com>
2016-02-05 22:39:22 -0500

    Add temporary responses

--- server.js
+++ server.js
@@ -14,10 +14,51 @@ let Options = {
 
 App.use(ServeStatic(`${__dirname}/public`));
 
+function replyTo(msg) {
+
+  function hasKeywords(keywords) {
+    if (typeof keywords == 'string') {
+      keywords = keywords.split(' ');
+    }
+    for (var i = 0; i < keywords.length; i--) {
+      if (msg.split(' ').indexOf(keywords[i]) === -1) {
+        return false;
+      }
+    }
+    return true;
+  }
+  function matches(regexp) {
+    if (msg.match(regexp)) {
+      return true;
+    }
+    return false;
+  }
+
+  if (hasKeywords('echo')) {
+    return msg.split('echo').pop();
+  }
+  if (matches(/.*(date today|day is it).*/)) {
+    return new Date().toDateString();
+  }
+  if (hasKeywords(`hello ${Options['alias']}`) {
+    return 'Hello!';
+  }
+
+  // Default case if we don't understand the message
+  var notFound = [
+    "I did not understand that",
+    "I didn't catch that",
+    "Would you please say that again",
+    "I don't know what that means",
+    "What did you say?"
+  ];
+  return notFound[Math.floor(Math.random() * notFound.length)];
+}
+
 Io.on('connection', (socket) => {
   socket.on('message', (msg) => {
     // This is where response logic will go
-    var reply = msg['contents'];
+    var reply = replyTo(msg['contents']);
     // ^ for the time being, we'll just echo back messages
     socket.emit('reply', reply);
   });
